15c1570cb9f5e23aacbbb055ffbeb4befefaf861
Yimin Zhong <yzz0225@auburn.edu>
2025-11-08 17:54:20 -0600

    Update: Configure single HTML file deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/department_game/',
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    minify: true,
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'everything.js'
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
